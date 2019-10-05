e9e63ab73e7d3c81961ef800e079934028562ef0
Ashton Creel <ashton@Ashtons-MacBook-Pro.local>
2019-10-05 17:25:26 -0600

    Add show object attr and search functions back to html

--- src/main/java/abac/spring/data/neo4j/repositories/ObjectAttributeRepository.java
+++ src/main/java/abac/spring/data/neo4j/repositories/ObjectAttributeRepository.java
@@ -3,9 +3,11 @@ package abac.spring.data.neo4j.repositories;
 import abac.spring.data.neo4j.domain.ObjectAttribute;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.Collection;
 
+@RepositoryRestResource(collectionResourceRel = "objectAttributes", path = "objectAttributes")
 public interface ObjectAttributeRepository extends Neo4jRepository<ObjectAttribute, Long> {
 	ObjectAttribute findByType(@Param("type") String type);
     Collection<ObjectAttribute> findByTypeLike(@Param("type") String type);
